8565016c550fbe3fefbd4510e410ee5bb58a9108
Nikita <nkgs370@gmail.com>
2023-10-10 20:33:30 +0300

    removed old libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import { EDF, readUploadedFile } from "./utils";
 import { Line } from "react-chartjs-2";
-import { labels, options } from "./components/charts/Chart";
+
+// SAMPLE SIZE / DURATION = 1/256 of seconds is my x axis
 
 function App() {
-  const [edf, setEdf] = useState<EDF>();
-  // console.log(edf?.readSingleChannel(0, 1, 5));
-
-  const startSecond = 1;
-  // const lengthSeconds = 5;
-
-  //Reading data from all channels [[], [], []]
-  // const data = edf.read(startSecond, edf.duration);
-  // console.log(data);
-
-  const handleChangeFile = async (event) => {
-    const file = event.target.files[0];
-
-    try {
-      const fileContents = await readUploadedFile(file);
-      // console.log(fileContents);
-
-      if (fileContents) {
-        let edfResult = new EDF(fileContents);
-        setEdf(edfResult);
-      }
-    } catch (e) {
-      console.warn(e.message);
-    }
-  };
-
   return (
     <div className="App">
       {edf ? (
