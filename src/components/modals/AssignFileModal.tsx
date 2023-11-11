4c64fcf1a580a15668d5789e39728e57bffc93bd
Nikita <nkgs370@gmail.com>
2023-11-11 18:30:13 +0200

    toast after assigning

--- src/components/modals/AssignFileModal.tsx
+++ src/components/modals/AssignFileModal.tsx
@@ -11,6 +11,9 @@ import { useCallback, useState } from "react";
 import styled from "styled-components";
 import { baseUrl, convertFilesToBase64 } from "../../utils";
 import api from "../../api/api";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AxiosResponse } from "axios";
 
 interface Props {
   open: boolean;
@@ -40,10 +43,12 @@ export const AssignFileModal = ({ open, handleCloseAssignModal }: Props) => {
   );
 
   const handleAssign = async () => {
-    await api.post(`${baseUrl}/assignFiles`, {
+    const response = await api.post(`${baseUrl}/assignFiles`, {
       email,
       files: fileStrings,
     });
+
+    handleNotify(response);
   };
 
   const handleCancel = () => {
@@ -51,48 +56,74 @@ export const AssignFileModal = ({ open, handleCloseAssignModal }: Props) => {
     handleCloseAssignModal();
   };
 
+  const handleNotify = (response: AxiosResponse<any, any>) => {
+    if (response.status === 200) {
+      toast("Successfully assigned!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } else {
+      toast(`${response.data.message}`);
+    }
+
+    handleCancel();
+  };
+
   return (
-    <Dialog open={open}>
-      <DialogContent>
-        <DialogTitle>Assign Files</DialogTitle>
-        <Column>
-          <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
-            <InputLabel>Email</InputLabel>
-            <OutlinedInput
-              label="Email"
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </FormControl>
-        </Column>
-        <Column>
-          <Button variant="contained" component="label">
-            Choose Files
-            <input type="file" onChange={handleFilesChange} multiple hidden />
-          </Button>
-          {fileStrings.length > 0 && (
-            <p>
-              {fileStrings.length === 1
-                ? `${fileStrings.length} file selected`
-                : `${fileStrings.length} files selected`}
-            </p>
-          )}
-        </Column>
-        <div
-          style={{ justifyContent: "space-between", display: "flex", flex: 1 }}
-        >
-          <Button
-            variant="outlined"
-            onClick={handleAssign}
-            disabled={!email || fileStrings.length === 0}
+    <>
+      <Dialog open={open}>
+        <DialogContent>
+          <DialogTitle>Assign Files</DialogTitle>
+          <Column>
+            <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+              <InputLabel>Email</InputLabel>
+              <OutlinedInput
+                label="Email"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </FormControl>
+          </Column>
+          <Column>
+            <Button variant="contained" component="label">
+              Choose Files
+              <input type="file" onChange={handleFilesChange} multiple hidden />
+            </Button>
+            {fileStrings.length > 0 && (
+              <p>
+                {fileStrings.length === 1
+                  ? `${fileStrings.length} file selected`
+                  : `${fileStrings.length} files selected`}
+              </p>
+            )}
+          </Column>
+          <div
+            style={{
+              justifyContent: "space-between",
+              display: "flex",
+              flex: 1,
+            }}
           >
-            Assign
-          </Button>
-          <Button variant="outlined" onClick={handleCancel}>
-            Cancel
-          </Button>
-        </div>
-      </DialogContent>
-    </Dialog>
+            <Button
+              variant="outlined"
+              onClick={handleAssign}
+              disabled={!email || fileStrings.length === 0}
+            >
+              Assign
+            </Button>
+            <Button variant="outlined" onClick={handleCancel}>
+              Cancel
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+      <ToastContainer />
+    </>
   );
 };
 
